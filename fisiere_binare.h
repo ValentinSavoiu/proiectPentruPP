5c78b516849ba61e440805f709cbe37f6a6c35ab
ValentinSavoiu <44209858+ValentinSavoiu@users.noreply.github.com>
2018-12-05 12:53:54 +0200

    Update fisiere_binare.h

--- fisiere_binare.h
+++ fisiere_binare.h
@@ -1,6 +1,6 @@
 #ifndef FISIERE_BINARE_H_INCLUDED
 #define FISIERE_BINARE_H_INCLUDED
-#define header_size 54
+#define HEADER_SIZE 54
 /*unsigned char* read_linearize(char *, unsigned char **, unsigned int, unsigned int);
 void save_image (char *, unsigned char *, unsigned int , unsigned int , unsigned char* );*/
 
@@ -9,8 +9,39 @@ unsigned char* read_linearize(char *fileName, unsigned char **header, unsigned i
     if (fin == NULL) {
         return NULL;
     }
-    *header = (unsigned char *) malloc(header_size * sizeof(unsigned char));
-    fread(*header, sizeof(unsigned char), header_size, fin); //asa da
+    
+    
+    *header = (unsigned char *) malloc(HEADER_SIZE * sizeof(unsigned char));
+    fread(*header, sizeof(unsigned char), HEADER_SIZE, fin); 
+    fseek(fin, 18, SEEK_SET);
+    fread(W, sizeof(unsigned int), 1, fin);
+    fread(H, sizeof(unsigned int), 1, fin);
+    
+    unsigned char padding;
+    if ( (*W) % 4)
+        padding = 4 - (3 * (*W)) % 4;
+    else
+        padding = 0;
+        
+    unsigned char *v = (unsigned char *) calloc( (*W) * (*H) * 3, sizeof(unsigned char));
+    fseek (fin, 54, SEEK_SET);
+    for (unsigned int i = 0; i < (*H); ++i) {
+        fread(v + (*H - i - 1)* 3 * (*W), sizeof(unsigned char), 3 * (*W), fin);
+        //fread(v + 3 * i * (*W), sizeof(unsigned char), 3 * (*W), fin);
+        fseek(fin, padding * sizeof(unsigned char), SEEK_CUR);
+    }
+    fclose(fin);
+    return v;
+}
+
+
+unsigned char *read_as_matrix(char *fileName, unsigned char **header, unsigned int *W, unsigned int *H) {
+	FILE *fin = fopen(fileName, "rb");
+    if (fin == NULL) {
+        return NULL;
+    }
+    *header = (unsigned char *) malloc(HEADER_SIZE * sizeof(unsigned char));
+    fread(*header, sizeof(unsigned char), HEADER_SIZE, fin); 
     fseek(fin, 18, SEEK_SET);
     fread(W, sizeof(unsigned int), 1, fin);
     fread(H, sizeof(unsigned int), 1, fin);
@@ -19,19 +50,43 @@ unsigned char* read_linearize(char *fileName, unsigned char **header, unsigned i
         padding = 4 - (3 * (*W)) % 4;
     else
         padding = 0;
-    unsigned char *v = (unsigned char *) calloc( (*W) * (*H) * 3, sizeof(unsigned char));
+    pixel *v = (pixel *) malloc( (*W) * (*H) * sizeof(pixel));
     fseek (fin, 54, SEEK_SET);
-    for (unsigned int i = 0; i < (*H); ++i) {
-        fread(v + (*H - i - 1)* 3 * (*W), sizeof(unsigned char), 3 * (*W), fin);
-        //fread(v + 3 * i * (*W), sizeof(unsigned char), 3 * (*W), fin);
+    for (unsigned int i = 0, i < (*H); ++i) {
+        for (unsigned int j = 0; j < (*W); ++j) {
+            fread(&v[H - i - 1][j].b, sizeof(unsigned char), 1, fin);
+            fread(&v[H - i - 1][j].g, sizeof(unsigned char), 1, fin);
+            fread(&v[H - i - 1][j].r, sizeof(unsigned char), 1, fin);
+            v[H - i - 1][j].grey = ( 0.299 * v[H - i - 1][j].r + 0.587 * v[H - i - 1][j].g + 0.114 * v[H - i - 1][j].b);
+        }
         fseek(fin, padding * sizeof(unsigned char), SEEK_CUR);
     }
+    fclose(fin);
     return v;
+}
+
+void save_image_matrix (char *fileName, pixel *A, unsigned int *W, unsigned int *H, unsigned char *header) {
+    FILE *fout = fopen(fileName, "wb");
+    fwrite(header, sizeof(unsigned char), HEADER_SIZE, fout);
+    unsigned char padding;
+    if (W % 4)
+        padding = 4 - (3 * W) % 4;
+    else
+        padding = 0;
+    for (unsigned int i = 0; i < H; ++i) {
+        for (unsigned int j = 0; j < W; ++j) {
+            fwrite(A[H - i - 1][j].b, sizeof(unsigned char), 1, fout);
+            fwrite(A[H - i - 1][j].g, sizeof(unsigned char), 1, fout);
+            fwrite(A[H - i - 1][j].r, sizeof(unsigned char), 1, fout);
+        } 
+        fseek(fin, padding * sizeof(unsigned char), SEEK_CUR);
+    }
+    fclose(fout);
 }
 
 void save_image (char *fileName, unsigned char *linearization, unsigned int W, unsigned int H, unsigned char *header) {
     FILE *fout = fopen(fileName, "wb");
-    fwrite(header, sizeof(unsigned char), header_size, fout);
+    fwrite(header, sizeof(unsigned char), HEADER_SIZE, fout);
     unsigned char padding;
     if (W % 4)
         padding = 4 - (3 * W) % 4;
@@ -41,5 +96,6 @@ void save_image (char *fileName, unsigned char *linearization, unsigned int W, u
         fwrite(linearization + (H - i - 1) * 3 * W, sizeof(unsigned char), 3 * W, fout);
         fseek(fout, padding * sizeof(unsigned char), SEEK_CUR);
     }
+    fclose(fout);
 }
 #endif // FISIERE_BINARE_H_INCLUDED
