ddcfd36c48fc69952f92fe6786ad3e7248ccaaec
ValentinSavoiu <44209858+ValentinSavoiu@users.noreply.github.com>
2018-12-05 12:55:47 +0200

    Update main.c

--- main.c
+++ main.c
@@ -11,9 +11,9 @@ int main() {
     char *encryptionDestination = (char*) malloc(30);
     scanf("%s30", encryptionDestination);
     printf("\nIntroduceti numele  fisierului in care se afla cele 2 chei secrete:");
-    char *secret_key = (char*) malloc(30);
-    scanf("%s30", secret_key);
-    int ok = encrypt(encryptionSource, encryptionDestination, secret_key);
+    char *secretKey = (char*) malloc(30);
+    scanf("%s30", secretKey);
+    int ok = encrypt(encryptionSource, encryptionDestination, secretKey);
     if (ok == 0) {
         printf("\nCriptarea s-a realizat cu succes");
     }
@@ -22,7 +22,7 @@ int main() {
     }
     free(encryptionDestination);
     free(encryptionSource);
-    free(secret_key);
+    free(secretKey);
 
     printf("\nIntroduceti numele imaginii de decriptat :");
     char *decryptionSource = (char*) malloc(30);
@@ -32,8 +32,8 @@ int main() {
     scanf("%s30", decryptionDestination);
     printf("\nIntroduceti numele  fisierului in care se afla cele 2 chei secrete, aceleasi chei folosite pentru criptarea initiala a imaginii :");
     secret_key = (char*) malloc(30);
-    scanf("%s30", secret_key);
-    ok = decrypt(decryptionSource, decryptionDestination, secret_key);
+    scanf("%s30", secretKey);
+    ok = decrypt(decryptionSource, decryptionDestination, secretKey);
     if (ok == 0) {
         printf("\nDecriptarea s-a realizat cu succes");
     }
